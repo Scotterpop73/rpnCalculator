968de83b4f97d856bc7fb3c907500bf55edd4b72
Scotterpop73 <sctzone@aol.com>
2023-05-17 11:46:05 -0700

    Added being able to continue a calculation

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//Step 1: Get the user's input - complete
-//Step 2: Validate user's input
+//Step 1: Get the user's input - complete (has to be all in one line)
+//Step 2: Validate user's input - complete
 //Step 3: If not valid input, have user input again - complete
 //Step 4: If valid input, do the calculation - thinking maybe to use recursion to do this - complete
 //Step 5: Output answer - complete
-//Step 6: Output option to quit or to do another calculation
+//Step 6: Output option to quit or to do another calculation complete
 
 //rpn follows the concept of a stack, which is a last in / first out concept
 
@@ -16,20 +16,31 @@ let userInputArray = [];
 let playAgain = true;
 let howManyNumbers = 0;
 let howManyOperators = 0;
+let previousSolution = 0;
+//Validation if user inputted a letter or special character
+//Going to use regex for data validation
+let correctChars = /^[\d+\-*/. ]+$/;
 
 
-while (playAgain) {
+while (playAgain){
     //boolean for the user's input, if their input is valid the value will flip to true to get out of the nested while loop
     let isInputValid = false;
 
     while(!isInputValid){
 
-        //Ask for the user to input their calculation
-        userInput = prompt('Enter your calculation with a space between each number and operator: ');
-        
-        //Validation if user inputted a letter or special character
-        //Going to use regex for data validation
-        let correctChars = /^[\d+\-*/. ]+$/;
+        //before asking for the user's calculation, check if there is a previous solution and ask the user if they want to use it
+        if(previousSolution !== 0){
+            const usePrevious = prompt("Do you want to use the previous solution? (y/n): ").toLowerCase();
+            if(usePrevious === 'y'){
+                //put the previousSolution and the user's input into the same string
+                userInput = previousSolution.toString() + ' ' + prompt('Enter the next operator and number: ');
+            }else{
+                userInput = prompt('Enter your calculation with a space between each number and operator: ');
+            }
+        }else{
+            //Ask for the user to input their calculation
+            userInput = prompt('Enter your calculation with a space between each number and operator: ');
+        }
         
         //Need to check if there is a correct amount of operators
         //Should always be one less operators than numbers inputted
@@ -65,16 +76,18 @@ while (playAgain) {
 
 
 //write a recursive function to pass in the userInputArray and keep doing a calculation until the length of the array is 1
-function rpnEvaluation(userInputArray){
+function rpnEvaluation(stack){
     //if there is only one number in the array, return that number
-    if(userInputArray.length === 1){
-        return console.log(parseFloat(userInputArray[0]));
+    if(stack.length === 1){
+        //storing this value in the userInputArray for the case the user wants to continue an operation
+        previousSolution = parseFloat(stack[0]);
+        return console.log(parseFloat(stack[0]));
     }
 
     //filter only the operators out of the userInputArray
-    let operatorArray = userInputArray.filter(char => isNaN(char));
+    let operatorArray = stack.filter(char => isNaN(char));
     //filter only the numbers out of the userInputArray
-    let numberArray = userInputArray.filter(char => !isNaN(char));
+    let numberArray = stack.filter(char => !isNaN(char));
     
     //logic for getting two numbers and operator to call the calculate function goes here
     //use parseFloat to convert from a string to a number
@@ -87,7 +100,7 @@ function rpnEvaluation(userInputArray){
     //remove the elements that were evaluated and add in the result into the array
     numberArray.splice(0, 2, result);
     //remove the first operator from the operatorArray
-    operatorArray.splice(0,1);
+    operatorArray.splice(0, 1);
 
     //concat the two arrays so that the operators are in the array
     const concatArray = numberArray.concat(operatorArray);
